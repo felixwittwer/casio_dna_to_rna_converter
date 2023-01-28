031dd6b56c22dbc7660ab720b704a74055d4a975
Felix Wittwer <120730441+felixwittwer@users.noreply.github.com>
2023-01-28 18:43:01 +0100

    Add printing DNA strand

--- DNA.c
+++ DNA.c
@@ -8,10 +8,12 @@
 /*                                                               */
 /*****************************************************************/
 #include "fxlib.h"
+#include <stdio.h>
 
-void DrawLineWithMarkers(int x, int y, int marker, int ends,char symbols[]){
+void RenderStrand(int x, int y, int marker, int ends, char symbols[]){
     int iteration = 0;
     int xaddition;
+    unsigned char buffer[12];
 
     Bdisp_DrawLineVRAM(x,y,x+102,y);
 
@@ -28,6 +30,15 @@ void DrawLineWithMarkers(int x, int y, int marker, int ends,char symbols[]){
         xaddition = (iteration)*5+(7*((iteration)/3))+2;
 
         Bdisp_DrawLineVRAM(x+xaddition,y,x+xaddition,y+marker);
+
+        sprintf(buffer, "%c", symbols[iteration]);
+        if(marker>0){
+            PrintMini(x+xaddition-1,y+3, buffer, MINI_OVER);
+        }else if(marker<0){
+            PrintMini(x+xaddition-1,y-7, buffer, MINI_OVER);
+        }
+
+
         iteration = iteration + 1;
     }
 }
@@ -55,7 +66,9 @@ void RendertRNA(int x, int y){
 int AddIn_main(int isAppli, unsigned short OptionNum)
 {
     unsigned int key;
-    char dna [12] = {'A','T','G','T','G','T','G','T','G','T','G','T'};
+    char dnaone [15] = "ATGTCGGTGCTGTAT";
+    char dnatwo [15] = "";
+    char mrna [15] = "";
 
     Bdisp_AllClr_DDVRAM();
 
@@ -63,15 +76,15 @@ int AddIn_main(int isAppli, unsigned short OptionNum)
     while(1){
         GetKey(&key);
 
-        DrawLineWithMarkers(12,2,1,5, dna);
-        DrawLineWithMarkers(12,22,-1,3, dna);
-        DrawLineWithMarkers(12,24,1,5, dna);
+        RenderStrand(12,2,1,5, dnaone);
+        RenderStrand(12,19,-1,3, dnatwo);
+        RenderStrand(12,21,1,5, mrna);
 
-        RendertRNA(19,44);
-        RendertRNA(41,44);
-        RendertRNA(63,44);
-        RendertRNA(85,44);
-        RendertRNA(107,44);
+        RendertRNA(19,38);
+        RendertRNA(41,38);
+        RendertRNA(63,38);
+        RendertRNA(85,38);
+        RendertRNA(107,38);
 
     }
 
